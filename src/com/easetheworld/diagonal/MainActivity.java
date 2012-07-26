2693b65e667480ead93a6ba011c360df8a71ca66
EaseTheWorld <easetheworld@gmail.com>
2012-07-26 12:05:39 +0900

    - Remove ZOProgressTextView and add ZOTouchViewController instead. - Build new MVC model with ViewController.

--- src/com/easetheworld/diagonal/MainActivity.java
+++ src/com/easetheworld/diagonal/MainActivity.java
@@ -1,13 +1,74 @@
 package com.easetheworld.diagonal;
 
+import dev.easetheworld.ui.zo.ZOTouchViewController;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	private TextView mTextView1;
+	private TextView mTextView2;
+	
+	private static final int VALUE_MIN = 0;
+	private static final int VALUE_MAX = 29;
+	private int mValue1;
+	private int mValue2;
+	
+	private ZOTouchViewController mZOController;
     
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        mTextView1 = (TextView)findViewById(android.R.id.text1);
+        mTextView2 = (TextView)findViewById(android.R.id.text2);
+        
+        mZOController = new ZOTouchViewController(this, mZODispatcher, 120);
+        
+        mZOController.addView(mTextView1);
+        mZOController.addView(mTextView2);
     }
+	
+	private ZOTouchViewController.Dispatcher mZODispatcher = new ZOTouchViewController.Dispatcher() {
+		
+		@Override
+		public void onDispatchZ(View v, int value) {
+			switch(v.getId()) {
+			case android.R.id.text1:
+				mValue1 = rotatedAdd(mValue1, value, VALUE_MIN, VALUE_MAX);
+				mTextView1.setText(String.valueOf(mValue1));
+				break;
+			case android.R.id.text2:
+				mValue2 = rotatedAdd(mValue2, value, VALUE_MIN, VALUE_MAX);
+				mTextView2.setText(String.valueOf(mValue2));
+				break;
+			}
+		}
+		
+		@Override
+		public void onDispatchO(View v, int value) {
+			switch(v.getId()) {
+			case android.R.id.text1:
+				mValue1 = rotatedAdd(mValue1, value, VALUE_MIN, VALUE_MAX);
+				mTextView1.setText(String.valueOf(mValue1));
+				break;
+			case android.R.id.text2:
+				mValue2 = rotatedAdd(mValue2, value, VALUE_MIN, VALUE_MAX);
+				mTextView2.setText(String.valueOf(mValue2));
+				break;
+			}
+		}
+	};
+	
+	private static int rotatedAdd(int a, int b, int min, int max) {
+		a += b;
+		if (a < min)
+			a += (max - min + 1);
+		else if (a > max)
+			a -= (max - min + 1);
+		return a;	
+	}
 }
--- src/dev/easetheworld/ui/zo/ZOTouchViewController.java
+++ src/dev/easetheworld/ui/zo/ZOTouchViewController.java
@@ -1,27 +1,29 @@
 package dev.easetheworld.ui.zo;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.util.AttributeSet;
 import android.util.FloatMath;
+import android.util.Log;
 import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
-import android.widget.TextView;
 
-import com.easetheworld.diagonal.R;
-
-public class ZOProgressTextView extends TextView {
+public class ZOTouchViewController {
+	
+	private static final String TAG = "ZOTouchViewController";
 	
     private static final int MODE_Z = 0;
     private static final int MODE_O = 1;
     private int mMode;
     
-	private int mMax = 0;
-	private float mDistanceSum = 0;
-	private int mDistanceThreshold = 20;
+    private static final int DIRECTION_FORWARD = 1;
+    private static final int DIRECTION_BACKWARD = -DIRECTION_FORWARD;
+    private int mDirection;
+    
+	private float mDistanceSum;
+	private int mDistanceThreshold;
+	private static final int DEFAULT_DISTANCE_THRESHOLD = 20;
     
     private PopupWindow mOverlayPopup;
     private ImageView mOverlay;
@@ -29,57 +31,69 @@ public class ZOProgressTextView extends TextView {
     private StrokeGestureDetector mStrokeDetector;
     
     private int[] mLocationXY = new int[2];
-
-	public ZOProgressTextView(Context context, AttributeSet attrs) {
-		super(context, attrs);
+    
+	private Dispatcher mDispatcher;
+	
+	public ZOTouchViewController(Context context, Dispatcher dispatcher) {
+		this(context, dispatcher, DEFAULT_DISTANCE_THRESHOLD);
+	}
+    
+	public ZOTouchViewController(Context context, Dispatcher dispatcher, int distanceThreshold) {
+		mDispatcher = dispatcher;
 		
-        mStrokeDetector = new StrokeGestureDetector(getContext(), mStrokeListener);
+        mStrokeDetector = new StrokeGestureDetector(context, mStrokeListener);
         mOverlay = new ImageView(context);
         mOverlay.setImageResource(android.R.drawable.btn_plus);
         mOverlayPopup = new PopupWindow(mOverlay);
         
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ZOProgressTextView); 
-        mMax = a.getInt(R.styleable.ZOProgressTextView_android_max, mMax);
-        mDistanceThreshold = a.getDimensionPixelSize(R.styleable.ZOProgressTextView_android_spacing, mDistanceThreshold);
+        mDistanceThreshold = distanceThreshold;
 	}
 	
-	public void setMax(int max) {
-		mMax = max;
+	public static interface Dispatcher {
+		void onDispatchZ(View v, int value);
+		void onDispatchO(View v, int value);
 	}
 	
-	public int getMax() {
-		return mMax;
+	private View mTouchedView;
+	
+	private View.OnTouchListener mOnTouchListener = new View.OnTouchListener() {
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			mTouchedView = v;
+			return mStrokeDetector.onTouchEvent(event);
+		}
+	};
+	
+	public void addView(View v) {
+		v.setOnTouchListener(mOnTouchListener);
 	}
-
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		if (mStrokeDetector.onTouchEvent(event))
-			return true;
-		else
-			return super.onTouchEvent(event);
+	
+	
+	public void removeView(View v) {
+		v.setOnTouchListener(null);
 	}
 	
 	private StrokeGestureDetector.OnStrokeGestureListener mStrokeListener = new StrokeGestureDetector.OnStrokeGestureListener() {
 		
 		@Override
 		public void onDown(MotionEvent e) {
-			android.util.Log.i("Stroke", "Down");
-			setStrokeIncreaseMount(1);
+			Log.i(TAG, "Down");
+			setDirection(DIRECTION_FORWARD);
 	        setMode(MODE_Z);
 		}
 		
 		@Override
 		public void onUp(MotionEvent e) {
-			android.util.Log.i("Stroke", "Up");
+			Log.i(TAG, "Up");
 	    	dismissPopupOnScreen();
 		}
 		
 		@Override
 		public boolean onStrokeStart(MotionEvent e) {
-			android.util.Log.i("Stroke", "Start "+e.getX()+","+e.getY());
+			Log.i(TAG, "Start "+e.getX()+","+e.getY());
 			switch(mMode) {
 			case MODE_Z:
-				addProgress(mStrokeIncreaseMount);
+				mDispatcher.onDispatchZ(mTouchedView, mDirection);
 				break;
 			case MODE_O:
 				mDistanceSum = 0;
@@ -90,13 +104,13 @@ public class ZOProgressTextView extends TextView {
 		
 		@Override
 		public boolean onStrokeMove(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-//			android.util.Log.i("Stroke", "Move e1="+e1+", e2="+e2+", dx="+distanceX+", dy="+distanceY);
+//			Log.i(TAG, "Move e1="+e1+", e2="+e2+", dx="+distanceX+", dy="+distanceY);
 	    	switch(mMode) {
 	    	case MODE_O:
 	    		mDistanceSum += FloatMath.sqrt(distanceX * distanceX + distanceY * distanceY);
 	    		int diff = (int)(mDistanceSum / mDistanceThreshold);
-				addProgress(mStrokeIncreaseMount * diff);
 				mDistanceSum = mDistanceSum % mDistanceThreshold;
+				mDispatcher.onDispatchO(mTouchedView, mDirection * diff);
 	    		break;
 	    	}
 	    	showPopupOnScreen((int)e2.getX(), (int)e2.getY());
@@ -105,10 +119,10 @@ public class ZOProgressTextView extends TextView {
 		
 		@Override
 		public boolean onStrokeEnd(MotionEvent e1, MotionEvent e2) {
-			android.util.Log.i("Stroke", "End "+e2.getX()+","+e2.getY()+" from "+e1.getX()+","+e1.getY());
+			Log.i(TAG, "End "+e2.getX()+","+e2.getY()+" from "+e1.getX()+","+e1.getY());
 	    	switch(mMode) {
 	    	case MODE_O:
-				setStrokeIncreaseMount(-mStrokeIncreaseMount);
+	    		setDirection(-mDirection);
 	    		break;
 	    	}
 			return false;
@@ -116,17 +130,17 @@ public class ZOProgressTextView extends TextView {
 
 		@Override
 		public boolean onSingleTapUp(MotionEvent e) {
-			android.util.Log.i("Stroke", "SingleTapUp "+e);
+			Log.i(TAG, "SingleTapUp "+e);
 			return false;
 		}
 		
 		@Override
 		public boolean onHold(float x, float y) {
-			android.util.Log.i("Stroke", "Hold");
+			Log.i(TAG, "Hold "+x+", "+y);
 			switch(mMode) {
 			case MODE_Z:
 				if (mStrokeDetector.isStroking())
-					setStrokeIncreaseMount(-mStrokeIncreaseMount);
+		    		setDirection(-mDirection);
 				else {
 					setMode(MODE_O);
 					showPopupOnScreen((int)x, (int)y);
@@ -136,30 +150,6 @@ public class ZOProgressTextView extends TextView {
 			return false;
 		}
 	};
-	
-	public int getProgress() {
-		return mProgress;
-	}
-	
-	public void setProgress(int progress) {
-		mProgress = progress;
-		setText(String.valueOf(mProgress));
-	}
-	
-	private int mProgress = 0;
-	
-	private int mStrokeIncreaseMount;
-	
-	private void addProgress(int diff) {
-		int progress = mProgress + diff;
-		if (mMax > 0) {
-			if (progress < 0)
-				progress += mMax + 1;
-			else if (progress > mMax)
-				progress -= mMax + 1;
-		}
-		setProgress(progress);
-	}
 	
 	private void setMode(int mode) {
 		mMode = mode;
@@ -173,18 +163,19 @@ public class ZOProgressTextView extends TextView {
 		}
 	}
 	
-	private void setStrokeIncreaseMount(int value) {
-		if (value > 0)
+	private void setDirection(int direction) {
+		if (direction > 0)
 			mOverlay.setImageResource(android.R.drawable.btn_plus);
 		else
 			mOverlay.setImageResource(android.R.drawable.btn_minus);
-		mStrokeIncreaseMount = value;
+		mDirection = direction;
 	}
+	
 	
     private void showPopupOnScreen(int x, int y) {
 		int popupWidth = mOverlay.getDrawable().getIntrinsicWidth();
 		int popupHeight = mOverlay.getDrawable().getIntrinsicHeight();
-		getLocationOnScreen(mLocationXY);
+		mTouchedView.getLocationOnScreen(mLocationXY);
 		int left = mLocationXY[0] + x - popupWidth / 2;
 		int top = mLocationXY[1] + y - popupHeight * 2;
     	if (mOverlayPopup.isShowing()) {
@@ -192,7 +183,7 @@ public class ZOProgressTextView extends TextView {
     	} else {
     		mOverlayPopup.setWidth(popupWidth);
     		mOverlayPopup.setHeight(popupHeight);
-    		mOverlayPopup.showAtLocation(this, Gravity.NO_GRAVITY, left, top);
+    		mOverlayPopup.showAtLocation(mTouchedView, Gravity.NO_GRAVITY, left, top);
     	}
     }
     
